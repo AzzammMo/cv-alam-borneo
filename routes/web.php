d69f26ad1be97e2ad9229385123849b5ae271516
Azzam <adinsjhay10@gmail.com>
2025-10-25 10:17:08 +0700

    update features: route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProdukController; // Tambahkan ini
+use App\Http\Controllers\ProdukController; 
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
